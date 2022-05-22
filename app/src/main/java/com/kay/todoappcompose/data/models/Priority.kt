84c43204171c282bf290cc248264b11187c180db
kay <kayshiro@live.no>
2022-05-22 17:12:33 +0200

    added model classes

---
+++ app/src/main/java/com/kay/todoappcompose/data/models/Priority.kt
@@ -0,0 +1,15 @@
+package com.kay.todoappcompose.data.models
+
+import androidx.compose.ui.graphics.Color
+import com.kay.todoappcompose.ui.theme.HighPriorityColor
+import com.kay.todoappcompose.ui.theme.LowPriorityColor
+import com.kay.todoappcompose.ui.theme.MediumPriorityColor
+import com.kay.todoappcompose.ui.theme.NonePriorityColor
+
+
+enum class Priority (val color: Color){
+    HIGH(HighPriorityColor),
+    MEDIUM(MediumPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(NonePriorityColor)
+}
---
+++ app/src/main/java/com/kay/todoappcompose/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.kay.todoappcompose.util
+
+// Define our Strings here?
+object Constants {
+    const val DATABASE_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_database"
+
+}
