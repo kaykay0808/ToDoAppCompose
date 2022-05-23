f9a14a0b4ed4060dbc19b2763778d1b8774a9ddd
kay <kayshiro@live.no>
2022-05-23 08:27:52 +0200

    Setup Dagger Hilt

---
+++ app/src/main/java/com/kay/todoappcompose/dependencyinjection/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.kay.todoappcompose.dependencyinjection
+
+import android.content.Context
+import androidx.room.Room
+import com.kay.todoappcompose.data.ToDoDatabase
+import com.kay.todoappcompose.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/** ==================== DATABASE MODULE ========================= */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
